e64b398b47046c0523d4738b788032726217e78b
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2024-09-28 17:55:38 +0100

    removed references to old code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { Page2 } from "./index2/static";
-
-/*
-export default async function Home () {
-    return <Page2/>
-}
-
-*/
-
 import { Speech2Txt } from "@/app/index/Speech2Txt";
 export default async function Home() {
     return <Speech2Txt/>
